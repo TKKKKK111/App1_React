7a26a534566fcd4a4ac7d3f02ec12b41cbe9ba57
Mirko Acevedo <mirko.acevedo.n@gmail.com>
2023-02-18 23:29:11 -0300

    Clean
    Limpieza de archivos que no usaré dentro del desarrollo de la app

---
+++ app1/src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = () => {
+    return (   
+           
+
+        <>
+        
+
+        <h1> Tittle </h1>
+
+        </>
+
+
+
+    );
+}
+ 
+export default Header;
